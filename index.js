7475ccab91bde29434e8939c0a1e7031208b2532
cernanb <cernanb@gmail.com>
2016-09-16 23:19:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking: function() {
+    setInterval(() => this.time += 1, 1000)
+  }
+}
+
+function censor(word, sentence) {
+  return sentence.split(word).join('BLEEP')
+}
+
+const violenceCensor = censor.bind(null, 'violence')
+const drugsCensor = censor.bind(null, 'drugs')
